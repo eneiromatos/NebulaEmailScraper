46213d7ea6660beb78ddeae48d32319029769cd7
eneiromatos <eneiromatos@gmail.com>
2022-05-02 23:24:10 -0400

    Add licensing information.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Nebula Email Scraper
+The main goal of this scraper is to extract emails from websites for use in email marketing campaigns. Its main feature is that you can segment using keywords so you can make a deep scraping by subject.
+
+![](screenshot.jpg)
+
+## Features
+* Made with Scrapy
+* Very precise Regex for email extraction.
+* Custom priority requests to not hit google too hard in a little time frame. 
+* Make use of a pipeline to clean all the data so just unique email addresses are retrieved.
+* Data output in a prettified "emails.json" JSON file.
+
+## TO DO
+* Save data in a database instead of a JSON File.
+* Make a proxy downloader middleware.
+* Add other search engines. 
+
+## Installation and running 
+To install and run this project copy or clone all the files to your preferred folder and type and execute:
+
+```bash
+pip install -r requirements.txt
+scrapy crawl google_email_spider
+```
+When the scrapy spider stops, a file called "emails.json" will be created in the root directory (where "scrapy.cfg" resides).
+
+Nebula Email Scraper was developed under `Python 3.10.4`using `Scrapy 2.6.1` and it's recommended to run in a virtual environment.
--- nebulaemailscraper/pipelines.py
+++ nebulaemailscraper/pipelines.py
@@ -1,3 +1,23 @@
+"""
+Nebula Email Scraper: is designed to scrape emails based on specific keyword
+using major search engines like Google.
+
+Copyright (C) 2021.  Eneiro A. Matos B.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import json
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
@@ -70,5 +90,5 @@ class EmailsDetailsPipeline:
                         self.saved_emails[index_domain]["emails"] = clean_emails
 
     def close_spider(self, spider):
-        with open('emails.json', 'w') as file:
+        with open("emails.json", "w") as file:
             file.write(json.dumps(self.saved_emails, indent=4))
